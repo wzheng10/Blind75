43d52d4bf8ff444b40e3a8992947c1924702e798
wzheng10 <wzheng100@gmail.com>
2021-11-22 17:06:48 -0800

    altPairs

---
+++ CodeBat/altPairs.java
@@ -0,0 +1,17 @@
+//Given a string, return a string made of the chars at indexes 0,1, 4,5, 8,9 ... so "kittens" yields "kien".
+//
+//        altPairs("kitten") → "kien"
+//        altPairs("Chocolate") → "Chole"
+//        altPairs("CodingHorror") → "Congrr"
+
+public String altPairs(String str) {
+    String word = "";
+    for (int i = 0; i < str.length(); i+=4) {
+        int end = i + 2;
+        if (end > str.length()) {
+            end = str.length();
+        }
+        word += str.substring(i, end);
+    }
+    return word;
+}
